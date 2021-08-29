c4e1265a07722afab35eda01a6da92a9f06e9dcd
cindyhjli <cindy.hongjia.li16@gmail.com>
2021-08-29 02:51:29 -0400

    update

--- food_waste_app/lib/RestaurantEditMeals.dart
+++ food_waste_app/lib/RestaurantEditMeals.dart
@@ -20,7 +20,6 @@ class _RestaurantHomepage extends State<RestaurantHomepage> {
           SizedBox(height: 45),
           title(),
           setStartTime(),
-          mealCounter(),
         ],
       ),
     );
@@ -45,28 +44,6 @@ class _RestaurantHomepage extends State<RestaurantHomepage> {
           _dateTime = time;
         });
       },
-    );
-  }
-}
-
-class mealCounter extends StatefulWidget {
-  @override
-  _mealCounter createState() => _mealCounter();
-}
-
-class _mealCounter extends State<RestaurantHomepage> {
-  int _mealCount = 0;
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text("Number of Meals"),
-      trailing: Row(
-        children: <Widget>[
-          _mealCount != 0? IconButton(icon: Icon(Icons.remove), onPressed: ()=>setState(()=>_mealCount--),): Container(),
-            Text(_mealCount.toString()),
-            IconButton(icon: Icon(Icons.add), onPressed: ()=>setState(()=>_mealCount++))
-        ]
-      )
     );
   }
 }
