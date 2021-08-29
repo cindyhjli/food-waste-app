7f42c766db23d04d13f14ea1a5a76541e96955ad
daniel-wang-design <daniel.wang04@gmail.com>
2021-08-29 01:53:56 -0400

    changed font

--- food_waste_app/lib/test.dart
+++ food_waste_app/lib/test.dart
@@ -6,6 +6,7 @@ import 'LoginPage.dart';
 import 'SearchPage.dart';
 import 'CustomerHomepage.dart';
 import 'RestaurantHomepage.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(Test());
@@ -15,7 +16,9 @@ class Test extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(fontFamily: "Montserrat"),
+      theme: ThemeData(
+        fontFamily: GoogleFonts.montserrat().fontFamily
+      ),
       debugShowCheckedModeBanner: false,
       debugShowMaterialGrid: false,
       home: RestaurantHomepage(),
